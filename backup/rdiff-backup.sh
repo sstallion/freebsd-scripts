6d27e29e3d0989879b784fdd964168db6e09a243
Steven Stallion <sstallion@gmail.com>
2025-02-07 16:14:52 -0600

    Sundry updates

--- backup/rdiff-backup.sh
+++ backup/rdiff-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # SPDX-License-Identifier: BSD-2-Clause
 #
-# Copyright (c) 2022 Steven Stallion <sstallion@gmail.com>
+# Copyright (c) 2025 Steven Stallion <sstallion@gmail.com>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
